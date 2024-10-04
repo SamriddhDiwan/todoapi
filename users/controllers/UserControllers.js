bfd4bebfc8d7ad32dded61ec4173c29289ff66c3
SamriddhDiwan <samriddhdiwan7@gmail.com>
2024-10-04 17:35:24 +0530

    patch updated

--- users/controllers/UserControllers.js
+++ users/controllers/UserControllers.js
@@ -9,13 +9,10 @@ const db = new pg.Client({
 });
 db.connect();
 
-
-
 export const addNew=async (req,res)=>{
     const user_id=req.body.user_id;
     const newTodo=req.body.todo;
     let result= await db.query("insert into todoList(user_id,todo) values($1,$2)",[user_id,newTodo]);
-    let todoList=[];
     return res.redirect('/user/all');
 }
 
@@ -34,13 +31,15 @@ export const getAll=async (req,res)=>{
 
 export const deleteTodo=async (req,res)=>{
     const user_id=req.body.user_id;
-    let result= await db.query("select todo,id from todoList where user_id=$1",[user_id]);
-    let todoList=[];
-    result.rows.forEach(todo => {
-        todoList.push([todo.id,todo.todo]);
-    });
-    return res.json({
-        status:true,
-        data:todoList
-    });
+    const id=req.body.id;
+    await db.query("delete from todoList where user_id=$1 AND id=$2",[user_id,id]);
+    return res.redirect('/user/all');
+}
+
+export const patchTodo=async (req,res)=>{
+    const user_id=req.body.user_id;
+    const newTodo=req.body.todo;
+    const id=req.body.id;
+    let result= await db.query("update todoList set todo =$2 where user_id=$1 AND id=$3 ",[user_id,newTodo,id]);
+    return res.redirect('/user/all')
 }
--- users/routes.js
+++ users/routes.js
@@ -1,21 +1,11 @@
 import  express from 'express';
-import { getAll,addNew,deleteTodo } from './controllers/UserControllers.js';
+import { getAll,addNew,deleteTodo, patchTodo } from './controllers/UserControllers.js';
 import { IsAuthenticatedMiddleware } from '../common/IsAuthenticatedMiddleware.js';
 const router=express.Router();
+
 router.get("/all", IsAuthenticatedMiddleware, getAll);
-
-
-
 router.post("/add",IsAuthenticatedMiddleware,addNew);
-
-
-
-router.patch("/user",(req,res)=>{
-    res.send("Under maintainance")
-});
-
-
-router.delete("/user",IsAuthenticatedMiddleware,deleteTodo);
-
+router.patch("/patch",IsAuthenticatedMiddleware,patchTodo);
+router.delete("/delete",IsAuthenticatedMiddleware,deleteTodo);
 
 export default router;
